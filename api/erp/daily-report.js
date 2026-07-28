60d361aa5c42845170b1fb6b18c6a0fbc9739dd8
fikrimamdouh <140603050+fikrimamdouh@users.noreply.github.com>
2026-07-28 19:41:40 +0300

    Use current-day-first ERP reconciliation

--- api/erp/daily-report.js
+++ api/erp/daily-report.js
@@ -1,3 +1,3 @@
-export { default } from '../_lib/daily-report-v5.js';
-export * from '../_lib/daily-report-v5.js';
+export { default } from '../_lib/daily-report-v6.js';
+export * from '../_lib/daily-report-v6.js';
 export { dailyParserEvidence,historicalSalesCompatibility,payloadFromAnalysis,postingDateForTransaction,resolveReportDate,splitAggregatedAnalysis } from '../_lib/daily-report-v3.js';
