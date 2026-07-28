315f8835f2bd29db74597986c05915f69db310ae
fikrimamdouh <140603050+fikrimamdouh@users.noreply.github.com>
2026-07-28 12:54:50 +0300

    Preserve ERP route helper exports

--- api/erp/daily-report.js
+++ api/erp/daily-report.js
@@ -1,2 +1,3 @@
 export { default } from './daily-report-v4.js';
 export * from './daily-report-v4.js';
+export { dailyParserEvidence,historicalSalesCompatibility,payloadFromAnalysis,postingDateForTransaction,resolveReportDate,splitAggregatedAnalysis } from './daily-report-v3.js';
