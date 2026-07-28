23c16a5725b229aeb691ec2a796829ea7001f9af
fikrimamdouh <140603050+fikrimamdouh@users.noreply.github.com>
2026-07-28 13:02:01 +0300

    Point ERP endpoint to private implementation

--- api/erp/daily-report.js
+++ api/erp/daily-report.js
@@ -1,3 +1,3 @@
-export { default } from './daily-report-v4.js';
-export * from './daily-report-v4.js';
-export { dailyParserEvidence,historicalSalesCompatibility,payloadFromAnalysis,postingDateForTransaction,resolveReportDate,splitAggregatedAnalysis } from './daily-report-v3.js';
+export { default } from '../_lib/daily-report-v4.js';
+export * from '../_lib/daily-report-v4.js';
+export { dailyParserEvidence,historicalSalesCompatibility,payloadFromAnalysis,postingDateForTransaction,resolveReportDate,splitAggregatedAnalysis } from '../_lib/daily-report-v3.js';
