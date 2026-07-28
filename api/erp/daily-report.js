a00d1c8f7b23659c1c850885623a706b8f065bb2
fikrimamdouh <140603050+fikrimamdouh@users.noreply.github.com>
2026-07-28 14:35:10 +0300

    Use immutable invoice and payment identity importer

--- api/erp/daily-report.js
+++ api/erp/daily-report.js
@@ -1,3 +1,3 @@
-export { default } from '../_lib/daily-report-v4.js';
-export * from '../_lib/daily-report-v4.js';
+export { default } from '../_lib/daily-report-v5.js';
+export * from '../_lib/daily-report-v5.js';
 export { dailyParserEvidence,historicalSalesCompatibility,payloadFromAnalysis,postingDateForTransaction,resolveReportDate,splitAggregatedAnalysis } from '../_lib/daily-report-v3.js';
