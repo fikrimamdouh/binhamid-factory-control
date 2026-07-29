c251905d234ead2dab271a7598218cea0e776604
fikrimamdouh <140603050+fikrimamdouh@users.noreply.github.com>
2026-07-29 10:20:04 +0300

    Route ERP daily reports through v7 repair

--- api/erp/daily-report.js
+++ api/erp/daily-report.js
@@ -1,3 +1,3 @@
-export { default } from '../_lib/daily-report-v6.js';
-export * from '../_lib/daily-report-v6.js';
+export { default } from '../_lib/daily-report-v7.js';
+export * from '../_lib/daily-report-v7.js';
 export { dailyParserEvidence,historicalSalesCompatibility,payloadFromAnalysis,postingDateForTransaction,resolveReportDate,splitAggregatedAnalysis } from '../_lib/daily-report-v3.js';
